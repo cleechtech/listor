796ed93625e46a1f77ee9a917166b4558999464e
Connor Leech <connorleech@gmail.com>
2016-03-20 18:37:40 -0700

    style update

--- public/js/directives/candidate.js
+++ public/js/directives/candidate.js
@@ -2,8 +2,10 @@
 
 app.directive('candidate', function(){
 	return {
-		template: '<h4>{{ item.displayName }} <i ng-click="editCandidate(item, listName)" class="fa fa-pencil-square-o"></i><span ng-click="deleteCandidate(item, listName)">X</span></h4>' +
+		template: '<div class="candidate">' +
+			'<h4>{{ item.displayName }} <i ng-click="editCandidate(item, listName)" class="fa fa-pencil-square-o"></i><span ng-click="deleteCandidate(item, listName)">X</span></h4>' +
 			'<p>{{ item.email }}, {{ item.phone }}</p>' +
-			'<p>{{ item.comments }}</p>'
+			'<p>{{ item.comments }}</p>' +
+			'</div>'
 	};
 });
